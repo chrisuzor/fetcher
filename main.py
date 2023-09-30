544eac7808c62ee6ad38c810de24e6e46ef0fc86
Christopher <christopher@ulesson.com>
2023-10-01 00:00:53 +0100

    Structuring the fetcher

---
+++ main.py
@@ -0,0 +1,36 @@
+import time
+
+import uvicorn as uvicorn
+from fastapi import FastAPI
+
+from config.celery_utils import create_celery
+from routers import mews
+
+
+def create_app() -> FastAPI:
+    current_app = FastAPI(title="Fetcher microservice which receives data from PMS ",
+                          description="Fetcher microservice which receives data from PMS and send to Parser Micro "
+                                      "service through celery and RabbitMQ",
+                          version="1.0.0", )
+
+    current_app.celery_app = create_celery()
+    current_app.include_router(mews.router)
+    return current_app
+
+
+app = create_app()
+celery = app.celery_app
+
+
+@app.middleware("http")
+async def add_process_time_header(request, call_next):
+    print('inside middleware!')
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(f'{process_time:0.4f} sec')
+    return response
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=9000, reload=True)
---
+++ schemas/mews.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel, Field
+from typing import Optional, List
+
+
+class MewsRoom(BaseModel):
+    """Message model for Mews Room data."""
+    room_number: str = Field(..., alias="RoomNumber")
+    room_id: str = Field(..., alias="RoomId")
+    room_name: str = Field(..., alias="RoomName")
+    room_description: str = Field(..., alias="RoomDescription")
+    status: str = Field(..., alias="Status")
+    is_cleaning_required: bool = Field(..., alias="IsCleaningRequired")
+    is_cleaning_in_progress: bool = Field(..., alias="IsCleaningInProgress")
+    is_cleaning_finished: bool = Field(..., alias="IsCleaningFinished")
+
+
+class MewsParse(BaseModel):
+    """Base model for Mews data."""
+
+    id: str = Field(..., alias="Id")
+    name: str = Field(..., alias="Name")
+    description: str = Field(..., alias="Description")
+    rooms: List[MewsRoom] = Field(..., alias="Rooms")
+    is_active: Optional[bool] = Field(..., alias="IsActive")
+
